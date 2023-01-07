e62b348bb9e61f2b24a13e511a15ecacbbbac1cf
Erik Kubiak <erikkubiak@gmail.com>
2023-01-07 18:32:15 +0100

    Basic player setup

---
+++ Assets/Scripts/Player/PlayerActions/PlantingBehavior.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Player.PlayerActions
+{
+    public class PlantingBehavior : MonoBehaviour
+    {
+        public void PlantSeed()
+        {
+            Debug.Log("Planting");
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerInventory.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class PlayerInventory : MonoBehaviour
+    {
+        
+    }
+    
+}
