3f95b23a4857915d7df35966fdc7d3c5a56f8592
Erik Kubiak <erikkubiak@gmail.com>
2023-01-07 10:01:59 +0100

    Add patrolling system + nav mesh data to the scene

---
+++ Assets/Scripts/AI/Patrol/RandomPatrolEntity.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Utilties;
+
+namespace AI.Patrol
+{
+    public class RandomPatrolEntity : PatrolEntity
+    {
+        [SerializeField] private FloatRange m_RadiusRange;
+        
+        // TODO: Move to utility
+        private Vector3 RandomOffset
+        {
+            get
+            {
+                float angle = Random.Range(0, 2 * Mathf.PI);
+                return new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * m_RadiusRange.RandomValue;
+            }
+        }
+
+        protected override Vector3 NextPosition => transform.position + RandomOffset;
+    }
+}
