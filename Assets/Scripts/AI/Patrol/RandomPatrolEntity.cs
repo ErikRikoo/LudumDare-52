01978b6b763e54002da51139607028507765ad2d
Erik Kubiak <erikkubiak@gmail.com>
2023-01-07 10:22:17 +0100

    Add angle sight for patroling

--- Assets/Scripts/AI/Patrol/RandomPatrolEntity.cs
+++ Assets/Scripts/AI/Patrol/RandomPatrolEntity.cs
@@ -6,13 +6,18 @@ namespace AI.Patrol
     public class RandomPatrolEntity : PatrolEntity
     {
         [SerializeField] private FloatRange m_RadiusRange;
+        [Range(0, 180)]
+        [SerializeField] private float m_AngleRange = 90;
+
+        private float RadianAngle => m_AngleRange * 2 * Mathf.Deg2Rad;
 
         // TODO: Move to utility
         private Vector3 RandomOffset
         {
             get
             {
-                float angle = Random.Range(0, 2 * Mathf.PI);
+                float radianAngle = RadianAngle;
+                float angle = Random.Range(-radianAngle, radianAngle);
                 return new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * m_RadiusRange.RandomValue;
             }
         }
@@ -29,6 +34,18 @@ namespace AI.Patrol
             
             GizmosUilities.DrawWireDisc(transform.position, m_RadiusRange.Min);
             GizmosUilities.DrawWireDisc(transform.position, m_RadiusRange.Max);
+            DrawGizmosAngleSight();
+        }
+
+        private void DrawGizmosAngleSight()
+        {
+            Vector3 matchRangeForward = transform.forward * m_RadiusRange.Max;
+
+            Gizmos.DrawLine(transform.position, 
+                transform.position + Quaternion.Euler(0, -m_AngleRange , 0) * matchRangeForward);
+            Gizmos.DrawLine(transform.position, 
+                transform.position + Quaternion.Euler(0, m_AngleRange , 0) * matchRangeForward);
+
         }
     }
 }
