3be32cc0d673d05d622997543a2df5765bd506cb
Erik Kubiak <erikkubiak@gmail.com>
2023-01-07 18:04:40 +0100

    Player movements included

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Player
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private PlayerMovement m_PlayerMovement;
+
+        public void OnMovement(InputAction.CallbackContext _context)
+        {
+            Debug.Log("On Movemen called");
+            m_PlayerMovement.WantedMovement = _context.ReadValue<Vector2>();
+            if (_context.phase is InputActionPhase.Canceled or InputActionPhase.Disabled)
+            {
+                m_PlayerMovement.WantedMovement = Vector2.zero;
+            }
+        }
+
+        // TODO: Do something else for mouse
+        public void OnControllerAimChanged(InputAction.CallbackContext _context)
+        {
+            
+        }
+
+        public void OnShoot(InputAction.CallbackContext _context)
+        {
+            
+        }
+
+        public void OnPlanting(InputAction.CallbackContext _context)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Utilties/Extensions/TransformExtension.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Utilities
+{
+    public static class TransformExtension
+    {
+        public static Transform LastChild(this Transform _instance)
+        {
+            Transform ret = _instance;
+            while (ret.childCount != 0)
+            {
+                Assert.AreEqual(ret.childCount, 1, "Last Child can only be called with string chain of transform");
+                ret = ret.GetChild(0);
+            }
+
+            return ret;
+        }
+
+        public static void MakeZLookAtDirection(this Transform _instance, Vector3 _direction)
+        {
+            var rot = _instance.rotation.eulerAngles;
+            rot.y = (Mathf.Atan2(_direction.y, _direction.x) - 0.5f *  Mathf.PI) * Mathf.Rad2Deg;
+            _instance.rotation = Quaternion.Euler(rot);
+        }
+    }
+}
