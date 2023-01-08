158d365232d5f288008bb0be38f137edace9931d
Gilberetzu <euri.herasme@gmail.com>
2023-01-08 08:19:46 -0400

    Added land plot generation Added simple land plot model placement

--- Assets/Scripts/PlantHandling/Planter.cs
+++ Assets/Scripts/PlantHandling/Planter.cs
@@ -1,18 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
+using Utilities;
 
 namespace PlantHandling
 {
     public class Planter : MonoBehaviour
     {
-        private PlantManager plantManager;
+        public PlantManager PlantManager;
         
-
         private Camera _mainCamera;
         private Plane _groundPlane;
         private Vector3 _lastMousePosition;
+        private bool _initialized;
+        [SerializeField]
+        private float landPlotPadding;
+        [SerializeField]
+        private int landPlotMaxCount;
+        [SerializeField]
+        private Vector2 landPlotGenerationRange;
+        [SerializeField]
+        private Vector2Int landPlotMinSize;
+        [SerializeField]
+        private Vector2Int landPlotMaxSize;
 
+        //Debug
+        [SerializeField]
+        private Material _debugMaterial;
 
         // Start is called before the first frame update
         void Start()
@@ -20,16 +35,40 @@ namespace PlantHandling
             //Get primitive cube mesh
             _mainCamera = Camera.main;
             _groundPlane = new UnityEngine.Plane(Vector3.up, Vector3.zero);
-        }
+            _initialized = true;
+            Assert.IsNotNull(PlantManager, "Plant Manager is null");
+            Assert.IsNotNull(_debugMaterial, "Debug Material is null");
 
-        void GenerateLandPlots()
-        {
-            while (true)
+            PlantManager.Initialize(landPlotMaxCount, landPlotGenerationRange, landPlotMinSize, landPlotMaxSize, landPlotPadding);
+            
+            var cubePrimitive = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cubePrimitive.GetComponent<MeshRenderer>().sharedMaterial = _debugMaterial;
+            GameObject.Destroy(cubePrimitive.GetComponent<BoxCollider>());
+
+            var baseColorID = Shader.PropertyToID("_BaseColor");
+            var plotMPB = new MaterialPropertyBlock();
+            plotMPB.SetColor(baseColorID, Color.green);
+            var slotMPB = new MaterialPropertyBlock();
+            slotMPB.SetColor(baseColorID, Color.red);
+
+            foreach (var landPlot in PlantManager.LandPlots)
             {
-                var randomPosition = Random.insideUnitCircle;
-                if (randomPosition.sqrMagnitude > 0.001f)
+                var landPlotGO = GameObject.Instantiate(cubePrimitive, transform);
+                landPlotGO.transform.position = landPlot.Rect.center.X0Y();
+                landPlotGO.transform.localScale = new Vector3(landPlot.Rect.width, 0.1f, landPlot.Rect.height);
+                landPlotGO.GetComponent<MeshRenderer>().SetPropertyBlock(plotMPB);
+
+                var startPosition = landPlot.Rect.position.X0Y() + new Vector3(PlantManager.cellSize * 0.5f, 0, PlantManager.cellSize * 0.5f);
+                for (int i = 0; i < landPlot.size.x; i++)
                 {
-                    randomPosition.Normalize();
+                    for (int j = 0; j < landPlot.size.y; j++)
+                    {
+                        var slotGO = GameObject.Instantiate(cubePrimitive, transform);
+                        var slotPosition = startPosition + new Vector3(i * PlantManager.cellSize, 0.012f, j * PlantManager.cellSize);
+                        slotGO.transform.position = slotPosition;
+                        slotGO.transform.localScale = new Vector3(PlantManager.cellSize * 0.85f, 0.1f, PlantManager.cellSize * 0.85f);
+                        slotGO.GetComponent<MeshRenderer>().SetPropertyBlock(slotMPB);
+                    }
                 }
             }
         }
@@ -50,8 +89,15 @@ namespace PlantHandling
         private void OnDrawGizmos()
         {
             Gizmos.DrawWireSphere(_lastMousePosition, 0.5f);
-            Gizmos.DrawWireSphere(Vector3.zero, 2.0f);
-
+            Gizmos.DrawWireSphere(Vector3.zero, landPlotGenerationRange.x);
+            Gizmos.DrawWireSphere(Vector3.zero, landPlotGenerationRange.y);
+            /*if (_initialized)
+            {
+                foreach (var landPlot in PlantManager.LandPlots)
+                {
+                    Gizmos.DrawWireCube(landPlot.Rect.center.X0Y(), landPlot.Rect.size.X0Y());
+                }
+            }*/ 
         }
     }
 }
