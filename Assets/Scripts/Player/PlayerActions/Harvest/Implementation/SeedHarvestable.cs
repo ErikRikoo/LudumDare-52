890ad4ee3b1cad3a0e0a5fd48b03ed1620d7a343
Erik Kubiak <erikkubiak@gmail.com>
2023-01-09 08:16:28 +0100

    Adding SeedHarvestable

---
+++ Assets/Scripts/Player/PlayerActions/Harvest/Implementation/SeedHarvestable.cs
@@ -0,0 +1,19 @@
+﻿using PlantHandling.PlantType;
+using UnityEngine;
+
+namespace Player.PlayerActions.Harvest.Implementation
+{
+    public class SeedHarvestable : MonoBehaviour, IHarvestable
+    {
+        public PlantType Seed;
+        public int Count;
+
+        public void OnHarvested(GameObject _harvester)
+        {
+            if (_harvester.TryGetComponent<PlayerStats>(out var stats))
+            {
+                stats.Inventory.AddSeed(Seed, Count);
+            }
+        }
+    }
+}
