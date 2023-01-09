0800060e14ba5c450336c112e4b77ccd2c8bdfe8
Gilberetzu <euri.herasme@gmail.com>
2023-01-09 13:19:58 -0400

    Added damageable to plant

---
+++ Assets/Scripts/PlantHandling/PlantDamageTrigger.cs
@@ -0,0 +1,21 @@
+using General;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantDamageTrigger : MonoBehaviour, IDamageable
+{
+    public float Health { get; set; }
+    public System.Guid id;
+    public event InformAttackersAboutDeath InformAboutDeath;
+    public LandPlot LandPlot { get; set; }
+
+    public void TakeDamage(float amount)
+    {
+        Health -= amount;
+        if (Health <= 0.1f)
+        {
+            LandPlot.PlantDestroyed(this.id);
+        }
+    }
+}
